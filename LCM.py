525e026293f4709259a10a0df3bdd6f8835fc874
Srishti Singh <47063263+srishti0908@users.noreply.github.com>
2019-02-13 00:21:51 +0530

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -3,30 +3,33 @@ Created on Wed Feb  6 22:24:17 2019
 
 @author: Srishti Singh
 """
-def lcm(a,b):
-    z=max(a,b)
+
+def lcm(num1,num2):
+    maxnum=max(num1,num2)
     l=[]
     m=[]
     n=[]
-    for i in range(1,z+1):
-        if a%i==0 and b%i==0:
+    for i in range(1,maxnum+1):
+        if num1%i==0 and num2%i==0:
             l.append(i)
-            a=a/i
-            b=b/i
-        if a%i==0 and b%i!=0:
+            num1=num1/i
+            num2=num2/i
+        if num1%i==0 and num2%i!=0:
             m.append(i)
-            a=a/i
-        if a%i!=0 and b%i==0:
+            num1=num1/i
+        if num1%i!=0 and num2%i==0:
             n.append(i)
-            b=b/i
-    print(l)
-    print(m)
-    print(n)
+            num2=num2/i
+    print("Common factors of Number 1 and Number 2:",l)
+    print("Remaining factors of Number 1:",m)
+    print("Remaining factors of Number 2:",n)
     lm=l+m+n
     print(lm)
     x=1
     for i in lm:
         x=x*i
     return(x)
-lcm(12,14) 
+number1=int(input("Enter First Number: "))
+number2=int(input("Enter Second Number: "))
+print("Your LCM is=",lcm(number1,number2))
 
